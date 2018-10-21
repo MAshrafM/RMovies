f497f2629a55979fc21d8d2ac0febd062a2bd3da
MAshraf <ee.mashraf@gmail.com>
2018-10-21 09:52:08 +0200

    movie bio componenet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Switch, Route } from 'react-router-dom';
 // Components
 import HomeComponent from './components/home-component/home-component.js';
 import SearchResultsComponent from './components/search-results-component/search-results-component.js';
+import MovieBioComponent from './components/movie-bio-component/movie-bio-component.js';
+
 // props
 const WrapperObj = (props) => {
   return props.children;
@@ -18,6 +20,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={HomeComponent} />
             <Route path="/search-results" component={SearchResultsComponent} />
+            <Route path="/movie-bio" component={MovieBioComponent} />
           </Switch>
         </WrapperObj>
       </div>
---
+++ src/components/movie-bio-component/movie-bio-component.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Row, Col } from 'react-bootstrap';
+
+import BackdropElement from './backdrop-element';
+import CreditsElement from './credits-element';
+import CurtainElement from './curtain-element';
+import PostContainerElement from './post-container-element';
+import ProductionCompaniesElement from './production-companies-element';
+import ProductionCountriesElement from './production-countries-element';
+import ReleaseRatingElement from './release-rating-element';
+
+class MovieBioComponent extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      releaseType: ["Premiere", "Theatrical (Limited)", "Theatrical", "Digital", "Physical", "TV"]
+    };
+  }
+  
+  render(){
+    let bgImg = "https://image.tmdb.org/t/p/original/7u3pxc0K1wx32IleAkLv78MKgrw.jpg";
+    return(
+      <div className="outer-border movie-bio">
+        <div className="movie-jumbo position__relative">
+          <CurtainElement />
+          <BackdropElement bgImg={bgImg} />
+          <PostContainerElement />
+        </div>
+        <div className="prod position__relative">
+          <Row className="show-grid">
+            <Col xs={12} sm={6} className="blocks">
+              <ProductionCompaniesElement />
+            </Col>
+            <Col xs={12} sm={6} className="blocks">
+              <ProductionCountriesElement />
+            </Col>
+          </Row>
+        </div>
+        <CreditsElement />
+        <ReleaseRatingElement />
+      </div>
+    )
+  }
+}
+
+export default MovieBioComponent;
